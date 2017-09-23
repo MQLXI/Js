544c4c457a24d51354afb997630666631020eae2
MQLXI <1510303058@qq.com>
2017-09-23 08:45:37 +0800

    mi

--- mi/js/index.js
+++ mi/js/index.js
@@ -1,3 +1,71 @@
+banner(3000);
+function banner(speed){
+	var index = 0;
+	var next = 0;
+	var white_ = 'rgba(255,255,255,0.8)';
+	var black_ = 'rgba(0,0,0,0.6)';
+	var flag = true;
+	var imgs = $('li', $('#banner_img'));
+	var imgPanel = $('.banner')[0];
+	var len = imgs.length;
+	var imgWidth = parseInt(getComputedStyle($('#banner_img'), null).width);
+	var btnLeft = $('#banner_img_left');
+	var btnRight = $('#banner_img_right');
+	for(var j = 0; j < len; j++){
+		$("#banner_point_list").innerHTML += '<li></li>';
+	}
+	var points = $('li', $('#banner_point_list'));
+	points[index].style.background = white_;
+	
+	var timer = setInterval(move, speed);
+	function move(direction, pos){
+		direction = direction || 'right';
+		next = direction == 'right' ? (next == len-1 ? 0 : ++next) : (next == 0 ? len-1 : --next);
+		next = pos != undefined ? pos : next;
+		direction == 'right' ? imgs[next].style.left = imgWidth+'px' : imgs[next].style.left = -imgWidth+'px';
+		if(direction == 'right'){
+			animate(imgs[index], {left:-imgWidth}, function(){flag = true;});
+			animate(imgs[next], {left:0}, function(){flag = true;});
+		}else{
+			animate(imgs[index], {left:imgWidth}, function(){flag = true;});
+			animate(imgs[next], {left:0}, function(){flag = true;});
+		}
+		points[index].style.background = black_;
+		points[next].style.background = white_;
+		index = next;
+	}
+	function moveTo(pos){
+		if(pos == index)
+			return;
+		pos > index ? move('right', pos) : move('left', pos);
+	}
+
+	imgPanel.onmouseover = function(){
+		clearInterval(timer);
+	}
+	imgPanel.onmouseout = function(){
+		timer = setInterval(move, speed);
+	}
+	btnLeft.onclick = function(){
+		if(flag){
+			move('left');
+			flag = false;
+		}
+	}
+	btnRight.onclick = function(){
+		if(flag){
+			 move('right');
+			 flag = false;
+		}
+	}
+
+	for(let i = 0; i < len; i++){
+		points[i].onclick = function(){
+			moveTo(i);
+		};
+	}
+}
+////////////////////////////////////////////////////////////////////
 clickTurn();
 function clickTurn(){
 	let moveUl = document.querySelectorAll('#moveUl');
@@ -12,7 +80,6 @@ function clickTurn(){
 		}
 	}
 	for(let i = 0; i < turn_right.length; i++) {
-		// let moveUl_len = Math.floor(moveUl[i].childNodes.length/2);
 		let moveUl_len = moveUl[i].getElementsByTagName('li').length;
 		turn_right[i].onclick = function(){
 			pos[i] = pos[i] > -296 * (moveUl_len - 1) ? pos[i] -= 296 : pos[i];
@@ -21,56 +88,83 @@ function clickTurn(){
 	}
 }
 ////////////////////////////////////////////////////////////////////////
-banner(3000);
-function banner(speed){
-	var index = 0;
-	var white_ = 'rgba(255,255,255,0.8)';
-	var black_ = 'rgba(0,0,0,0.6)'
-	var imgs = $('li', $('#banner_img'));
-	var imgPanel = $('.banner')[0];
-	var len = imgs.length;
-	var btnLeft = $('#banner_img_left');
-	var btnRight = $('#banner_img_right');
-	for(var j = 0; j < len; j++){
-		$("#banner_point_list").innerHTML += '<li></li>';
-	}
-	var points = $('li', $('#banner_point_list'));
-	points[index].style.background = white_;
-	imgs[0].style.zIndex = 2;
+slide();
+
+function slide(){
+	let slidePanel_1 = document.querySelector('.xm-plain-box .xm_star_box main ul');
+	let slidePanel_2 = document.querySelector('.recommend .recommend_list ul');
+	let btnLeft = document.querySelectorAll('.hg_more .more_left');
+	let btnRight = document.querySelectorAll('.hg_more .more_right');
+	let hg_more = document.querySelectorAll('.hg_more');
+	let flag = true;
+	let dir_1 = false, dir_2 = false;
 	
-	var timer = setInterval(move, speed);
-	function move(direction){
+	btnLeft[0].onclick = function(){
+		dir_1 = go(slidePanel_1, 0, false);
+	}
+	btnRight[0].onclick = function(){
+		dir_1 = go(slidePanel_1, 0, true);
+	}
+	btnLeft[1].onclick = function(){
+		dir_2 = go(slidePanel_2, 1, true);
+	}
+	btnRight[1].onclick = function(){
+		dir_2 = go(slidePanel_2, 1, false);
+	}
+	let timer_1 = setInterval(()=>{
+		dir_1 = go(slidePanel_1, 0, dir_1);
+	}, 8000);
+	let timer_2 = setInterval(()=>{
+		dir_2 = go(slidePanel_2, 1, dir_2);
+	}, 8000);
+	hg_more[0].onmouseover = function(){
+		clearInterval(timer_1);
+	}
+	hg_more[0].onmouseout = function(){
+		timer_1 = setInterval(()=>{
+			dir_1 = go(slidePanel_1, 0, dir_1);
+		}, 8000);
+	}
+	hg_more[1].onmouseover = function(){
+		clearInterval(timer_2);
+	}
+	hg_more[1].onmouseout = function(){
+		timer_2 = setInterval(()=>{
+			dir_2 = go(slidePanel_2, 1, dir_2);
+		}, 8000);
+	}
+
+	function move(slidePanel, direction, step){
 		direction = direction || 'right';
-		imgs[index].style.zIndex = 1;
-		points[index].style.background = black_;
-		index = direction == 'right' ? (index == len-1 ? 0 : ++index) : (index == 0 ? len-1 : --index);
-		imgs[index].style.zIndex = 2;
-		points[index].style.background = white_;
+		let lefts = parseInt(getComputedStyle(slidePanel, null).left);
+		step = direction == 'right' ? -step : step;
+		animate(slidePanel, {left: lefts + step}, ()=>{flag = true;});
 	}
-	function moveTo(pos){
-		imgs[index].style.zIndex = 1;
-		points[index].style.background = black_;
-		index = pos;
-		imgs[index].style.zIndex = 2;
-		points[index].style.background = white_;
-	}
-
-	imgPanel.onmouseover = function(){
-		clearInterval(timer);
-	}
-	imgPanel.onmouseout = function(){
-		timer = setInterval(move, speed);
-	}
-	btnLeft.onclick = function(){
-		move('left');
-	}
-	btnRight.onclick = function(){
-		move('right');
-	}
-
-	for(let i = 0; i < len; i++){
-		points[i].onclick = function(){
-			moveTo(i);
-		};
+	function go(obj, id, dir){
+		let pos = parseInt(getComputedStyle(obj, null).left);
+		let widths = parseInt(getComputedStyle(obj, null).width);
+		if(dir){
+			if((pos + widths) <= 1240){
+				btnLeft[id].style.color = '#ccc';
+				btnRight[id].style.color = '#666';
+				dir = false;
+			}
+			if(flag && pos < 0){
+				move(obj, 'left', 1240);
+				flag = false;
+			}
+		}else{
+			if(pos >= 0){
+				btnRight[id].style.color = '#ccc';
+				btnLeft[id].style.color = '#666';
+				dir = true;
+			}
+			if(flag && (pos + widths) > 1240){
+				move(obj, 'right', 1240);
+				flag = false;
+			}
+		}
+		return dir;
 	}
 }
+////////////////////////////////////////////////////////////////////////
--- mi/js/myjs.js
+++ mi/js/myjs.js
@@ -2,8 +2,19 @@
 * @Author: admin
 * @Date:   2017-09-19 17:58:22
 * @Last Modified by:   admin
-* @Last Modified time: 2017-09-19 18:03:04
+* @Last Modified time: 2017-09-22 17:08:52
 */
+/**
+ * $(select, range);
+ * getClasses(className, range);
+ * Floating(obj);    构造函数，让元素在页面内漂浮移动
+ * children(parent);    返回指定父元素下的所有子元素节点
+ * next(element, tagName)    查找指定标签名的最近下一个兄弟节点
+ * closed(element, tagName);    查找指定标签名的最近父节点
+ */
+
+
+
 function $(select, range){
 	range = range || document;
 	var first = select.charAt(0);
@@ -32,5 +43,114 @@ function getClasses(className, range){/*选择对象range下面的元素*/
 			}
 		}
 		return newarr;
+	}
+}
+
+function Floating(obj){
+	this.obj = obj;
+	this.maxY = window.innerHeight - this.obj.offsetHeight;
+	this.maxX = window.innerWidth - this.obj.offsetWidth;
+	this.stepX = 10;
+	this.stepY = 10;
+	this.timer;
+}
+Floating.prototype = {
+	start : function(){
+		this.move();
+	},
+	stop : function(){
+		clearInterval(this.timer);
+	},
+	move : function(){
+		let that = this;
+		this.timer = setInterval(function(){
+			that.maxY = window.innerHeight - that.obj.offsetHeight;
+			that.maxX = window.innerWidth - that.obj.offsetWidth;
+			let tops = that.obj.offsetTop;
+			let lefts = that.obj.offsetLeft;
+			if(tops >= that.maxY){
+				tops = that.maxY;
+				that.stepY *= -1;
+			}
+			if(tops <= 0){
+				tops = 0;
+				that.stepY *= -1;
+			}
+			if(lefts >= that.maxX){
+				lefts = that.maxX;
+				that.stepX *= -1;
+			}
+			if(lefts <= 0){
+				lefts = 0;
+				that.stepX *= -1;
+			}
+			that.obj.style.top = tops + that.stepY + 'px';
+			that.obj.style.left = lefts + that.stepX + 'px';
+		}, 100);
+	},
+	resize : function(){
+		this.maxY = window.innerHeight - this.obj.offsetHeight;
+		this.maxX = window.innerWidth - this.obj.offsetWidth;
+	}
+};
+
+function children(parent){
+	let childElement = [];
+	let child = parent.childNodes;
+	childElement = Array.prototype.filter.call(child, (element)=>{
+		return element.nodeType == 1;
+	});
+	return childElement;
+}
+/*查找指定标签名的最近下一个兄弟节点*/
+function next(element, tagName){
+	tagName = tagName.toUpperCase();
+	let nextElement = null;
+	do{
+		nextElement = element.nextElementSibling;
+		element = nextElement;
+	}while(nextElement && nextElement.nodeName != tagName)
+	return nextElement;
+}
+/*查找指定元素后面所有的兄弟节点*/
+function next(element){
+	let nextall = [];
+	let nextElement = null;
+	while(nextElement = element.nextElementSibling){
+		nextall.push(nextElement);
+		element = nextElement;
+	}
+	return nextall;
+}
+/*查找指定标签名的最近父节点*/
+function closed(element, tagName){
+	tagName = tagName.toUpperCase();
+	let parent = null;
+	do{
+		parent = element.parentNode;
+		element = parent;
+	}while(parent && parent.nodeName != tagName)
+	return parent;
+}
+/*指定元素后面插入新的元素*/
+HTMLElement.prototype.insertAfter = function(element, position){
+	let next = position.nextElementSibling;
+	if(next){
+		this.insertBefore(element, next);
+	}else{
+		this.appendChild(element);
+	}
+}
+/*把元素插入到指定父元素内*/
+HTMLElement.prototype.appendTo = function(element){
+	element.appendChild(this);
+}
+/*把新元素插入到子元素的最前面*/
+HTMLElement.prototype.appendPreviousChild = function(element){
+	let first = this.firstElementChild;
+	if(first){
+		this.insertBefore(element, first);
+	}else{
+		this.appendChild(element);
 	}
 }
